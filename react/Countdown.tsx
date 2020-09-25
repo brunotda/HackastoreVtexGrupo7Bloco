ced94aa912908a38b70c37f416c3a3b29569b885
marcoshenriquemaia <henriquemarcosmaia@gmail.com>
2020-09-24 21:10:38 -0300

    fix: respositorio

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,49 +1,16 @@
-import React, { useState } from "react";
-import { TimeSplit } from "./typings/global";
-import { tick, getTwoDaysFromNow } from "./utils/time";
-import { useCssHandles } from "vtex.css-handles";
+import React from 'react'
 
-const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
+interface CountdownProps { }
 
-interface CountdownProps {
-  targetDate: string;
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+  return <div></div>
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate = DEFAULT_TARGET_DATE,
-}) => {
-
-  const CSS_HANDLES = ["container"];
-
-  const [timeRemaining, setTime] = useState<TimeSplit>({
-    hours: "00",
-    minutes: "00",
-    seconds: "00",
-  });
-
-  const handles = useCssHandles(CSS_HANDLES);
-
-  tick(targetDate, setTime);
-
-  return (
-      <div className={`${handles.countdown} db tc`}>
-        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-      </div>
-  );
-};
-
 Countdown.schema = {
-  title: "editor.countdown.title",
-  description: "editor.countdown.description",
-  type: "object",
-  properties: {
-    targetDate: {
-      title: "Data final",
-      description: "Data final utilizada no contador ",
-      type: "string",
-      default: null,
-    },
-  },
-};
+  title: 'editor.countdown.title',
+  description: 'editor.countdown.description',
+  type: 'object',
+  properties: {},
+}
 
-export default Countdown;
+export default Countdown
